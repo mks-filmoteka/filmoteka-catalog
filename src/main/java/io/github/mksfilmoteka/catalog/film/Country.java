f8a28825c358ba6c36a56d8d07b79f88bb9e0d80
Maks <maxsouldrake@gmail.com>
2026-08-26 15:47:00 +0200

    fix collection search filtering

--- src/main/java/io/github/mksfilmoteka/catalog/film/Country.java
+++ src/main/java/io/github/mksfilmoteka/catalog/film/Country.java
@@ -1,7 +1,9 @@
 package io.github.mksfilmoteka.catalog.film;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import io.swagger.v3.oas.annotations.media.Schema;
+import tools.jackson.databind.exc.InvalidFormatException;
 
 @Schema(description = "Film countries of origin")
 public enum Country {
@@ -46,5 +48,18 @@ public enum Country {
     @JsonValue
     public String getJsonValue() {
         return jsonValue;
+    }
+
+    @JsonCreator
+    public static Country fromJson(String value) {
+        if (value == null) {
+            return null;
+        }
+        for (Country country : values()) {
+            if (country.name().equals(value) || country.jsonValue.equals(value)) {
+                return country;
+            }
+        }
+        throw InvalidFormatException.from(null, "Invalid country value", value, Country.class);
     }
 }
